02070a29e71f92fcbb4e5ba298b42fdf76df8d5b
Andrew <liu.andrew.x@gmail.com>
2016-04-04 14:50:04 -0400

    Multi-process executable

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "Config.h"
 #include "ParseDNS.h"
@@ -198,101 +200,119 @@ int main(int argc, char **argv) {
 
   int eStart = (argc >= 4) ? atoi(argv[3]) : 0;
   int eEnd = (argc == 5) ? atoi(argv[4]) : numEntries;
+
+  int child_processes = 0;
   for(int e = eStart; e < eEnd; e++) {
     if(entries[e]->d_type == DT_REG) {
-      uint64_t startProcTime = getTimeMilliseconds(); 
-
-      char filePath[512]; 
-      sprintf(filePath, "%s/%s", argv[1], entries[e]->d_name);
-
-      static double totalProcTime = 0; 
-      double perFileProcTime = (e > eStart) ? (totalProcTime / (e - eStart)) : 0; 
-      printf("\rProcessing file %s [%04d/%04d] (Avg Proc Time = %lf ms)",
-             filePath, (e - eStart), (eEnd - eStart), perFileProcTime); 
-      fflush(stdout);
-
-      char pcapErrorMsg[PCAP_ERRBUF_SIZE] = { 0 };
-      pcap_t *pcap = pcap_open_offline(filePath, pcapErrorMsg);
-      if(pcap == NULL) {
-        fprintf(stderr, "Could not open '%s' with pcap - %s\n", filePath,
-                pcapErrorMsg);
+      pid_t child_pid;
+      if ((child_pid = fork()) < 0) {
+        fprintf(stderr, "Failed to fork\n");
         exit(1);
       }
+      if (child_pid) {
+        child_processes++;
+      } else {
+        uint64_t startProcTime = getTimeMilliseconds();
 
-      struct bpf_program bpf;
-      if(pcap_compile(pcap, &bpf, "udp port 53 and (dst host " OLD_ADDRESS_STR
-                      " or dst host " NEW_ADDRESS_STR " or src host "
-                      OLD_ADDRESS_STR " or src host " NEW_ADDRESS_STR ")",
-                      1, 0) < 0) {
-        fprintf(stderr, "Could not compile filter - %s\n", pcap_geterr(pcap));
-        exit(1);
-      }
-      if(pcap_setfilter(pcap, &bpf) < 0) {
-        fprintf(stderr, "Could not set filter - %s\n", pcap_geterr(pcap));
-        exit(1);
-      }
-      pcap_freecode(&bpf);
+        char filePath[512];
+        sprintf(filePath, "%s/%s", argv[1], entries[e]->d_name);
 
-      int datalinkType = pcap_datalink(pcap);
+        static double totalProcTime = 0;
+        double perFileProcTime = (e > eStart) ? (totalProcTime / (e - eStart)) : 0;
+        printf("\rProcessing file %s [%04d/%04d] (Avg Proc Time = %lf ms)",
+               filePath, (e - eStart), (eEnd - eStart), perFileProcTime);
+        fflush(stdout);
 
-      int datalinkOffset;
-      switch(datalinkType) {
-        case DLT_LINUX_SLL:
-          datalinkOffset = 16;
-          break;
-        case DLT_EN10MB:
-          datalinkOffset = 14;
-          break;
-        case DLT_IEEE802:
-          datalinkOffset = 22;
-          break;
-        case DLT_NULL:
-          datalinkOffset = 4;
-          break;
-        case DLT_SLIP:
-        case DLT_PPP:
-          datalinkOffset = 24;
-          break;
-        case DLT_RAW:
-          datalinkOffset = 0;
-          break;
-        default:
-          fprintf(stderr, "Unknown datalink type %d\n", datalinkType);
+        char pcapErrorMsg[PCAP_ERRBUF_SIZE] = { 0 };
+        pcap_t *pcap = pcap_open_offline(filePath, pcapErrorMsg);
+        if(pcap == NULL) {
+          fprintf(stderr, "Could not open '%s' with pcap - %s\n", filePath,
+                  pcapErrorMsg);
           exit(1);
-      }
-
-      isFirstCapturePacket = true;
-
-      if(pcap_loop(pcap, -1, handlePacket, (uint8_t *)&datalinkOffset) < 0) {
-        fprintf(stderr, "Call to pcap_loop() failed - %s\n", pcap_geterr(pcap));
-        exit(1);
-      }
-
-      // Going through the list to check if we can process any more packets, with
-      // the relaxed ordering constraint now that we have no more to add/pair
-      while(packetProc < packetAdd) {
-        if(packets[packetProc].ready) {
-          processQueryResponse(&packets[packetProc]);
         }
-        packetProc++;
+
+        struct bpf_program bpf;
+        if(pcap_compile(pcap, &bpf, "udp port 53 and (dst host " OLD_ADDRESS_STR
+                        " or dst host " NEW_ADDRESS_STR " or src host "
+                       OLD_ADDRESS_STR " or src host " NEW_ADDRESS_STR ")",
+                       1, 0) < 0) {
+          fprintf(stderr, "Could not compile filter - %s\n", pcap_geterr(pcap));
+          exit(1);
+        }
+        if(pcap_setfilter(pcap, &bpf) < 0) {
+          fprintf(stderr, "Could not set filter - %s\n", pcap_geterr(pcap));
+          exit(1);
+        }
+        pcap_freecode(&bpf);
+
+        int datalinkType = pcap_datalink(pcap);
+
+        int datalinkOffset;
+        switch(datalinkType) {
+          case DLT_LINUX_SLL:
+            datalinkOffset = 16;
+            break;
+          case DLT_EN10MB:
+            datalinkOffset = 14;
+            break;
+          case DLT_IEEE802:
+            datalinkOffset = 22;
+            break;
+          case DLT_NULL:
+            datalinkOffset = 4;
+            break;
+          case DLT_SLIP:
+          case DLT_PPP:
+            datalinkOffset = 24;
+            break;
+          case DLT_RAW:
+            datalinkOffset = 0;
+            break;
+          default:
+            fprintf(stderr, "Unknown datalink type %d\n", datalinkType);
+            exit(1);
+        }
+
+        isFirstCapturePacket = true;
+
+        if(pcap_loop(pcap, -1, handlePacket, (uint8_t *)&datalinkOffset) < 0) {
+          fprintf(stderr, "Call to pcap_loop() failed - %s\n", pcap_geterr(pcap));
+          exit(1);
+        }
+
+        // Going through the list to check if we can process any more packets, with
+        // the relaxed ordering constraint now that we have no more to add/pair
+        while(packetProc < packetAdd) {
+          if(packets[packetProc].ready) {
+            processQueryResponse(&packets[packetProc]);
+          }
+          packetProc++;
+        }
+        packetAdd = 0;
+        packetProc = 0;
+
+        // Keeping track of captured time
+        isFirstCapture = false;
+        lastCaptureTime = (captureLastTime - captureStartTime);
+        totalCaptureTime += lastCaptureTime;
+        fprintf(capturelenLog, "%lu\n", lastCaptureTime);
+
+        uint64_t endProcTime = getTimeMilliseconds();
+        totalProcTime += (endProcTime - startProcTime);
+
+        numEntriesParsed++;
+        pcap_close(pcap);
+
+        // Terminate child process.
+        exit(0);
       }
-      packetAdd = 0;
-      packetProc = 0;
-
-      // Keeping track of captured time
-      isFirstCapture = false;
-      lastCaptureTime = (captureLastTime - captureStartTime);
-      totalCaptureTime += lastCaptureTime;
-      fprintf(capturelenLog, "%lu\n", lastCaptureTime);
-
-      uint64_t endProcTime = getTimeMilliseconds();
-      totalProcTime += (endProcTime - startProcTime);
-
-      numEntriesParsed++;
-      pcap_close(pcap);
     }
-
     free(entries[e]);
+  }
+
+  // Reap all child processes.
+  for (int i = 0; i < child_processes; i++) {
+    wait(NULL);
   }
 
   free(entries);
