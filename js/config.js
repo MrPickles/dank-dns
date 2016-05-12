76359c7b07c54afc1cc6194df26c34f8703e5855
Zhi Xiang Lin <zhixiang94@gmail.com>
2016-05-12 16:45:36 -0400

    Check DB for files before insert

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   db : {
     url : 'mongodb://localhost/dns',
-    collection : 'dns'
+    collection : 'dns',
+    filesCollection : 'files' // used to make sure no duplicates
   },
   cacheSize : 1000
 };
