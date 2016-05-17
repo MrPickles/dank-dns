24f215910a06b5dfc4a389a096ecc43de0a94277
Andrew Liu <liu.andrew.x@gmail.com>
2016-05-17 17:56:24 -0400

    Parse DNS header

--- betterC/src/dns.c
+++ betterC/src/dns.c
@@ -1,7 +1,20 @@
 #include "dns.h"
 
 int parseDNS(dns_t *out, const uint8_t *packet, const uint16_t size) {
-  // TODO(aliu1): Implement.
-  return -1;
+  // Check for valid header size or if packet is a query.
+  if (size < 16 || !out || !packet || !(packet[2] >> 7)) {
+    return -1;
+  }
+
+  // Set header fields.
+  out->header.id = ntohs(*((uint16_t *)packet));
+  out->header.flags1 = packet[2];
+  out->header.flags2 = packet[3];
+  out->header.qdcount = ntohs(*((uint16_t *)(packet + 4)));
+  out->header.ancount = ntohs(*((uint16_t *)(packet + 6)));
+  out->header.nscount = ntohs(*((uint16_t *)(packet + 8)));
+  out->header.arcount = ntohs(*((uint16_t *)(packet + 10)));
+
+  return 0;
 }
 
