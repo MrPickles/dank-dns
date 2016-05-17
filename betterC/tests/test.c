bbec49ed4880c071e3f0e1c7a1bffa8e9dd29dc8
Andrew Liu <liu.andrew.x@gmail.com>
2016-05-17 18:16:47 -0400

    Start testing framework

---
+++ betterC/tests/test.c
@@ -0,0 +1,23 @@
+#include "test.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+void print_state(char *name, int val) {
+  if (name) {
+    printf("%s: ", name);
+  }
+  if (val == 0) {
+    printf(ANSI_COLOR_RED "Failed\n" ANSI_COLOR_RESET);
+    exit(EXIT_FAILURE);
+  }
+  printf(ANSI_COLOR_GREEN "Passed\n" ANSI_COLOR_RESET);
+}
+
+void print_section(char *title) {
+  printf("------------\n%s\n------------\n", title);
+}
