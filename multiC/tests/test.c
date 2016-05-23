ef42259d8bba3624c25920bf88696895d6a95d15
Andrew Liu <liu.andrew.x@gmail.com>
2016-05-23 17:11:57 -0400

    Move ANSI color definitions to test header file

--- multiC/tests/test.c
+++ multiC/tests/test.c
@@ -3,10 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-
 void print_state(char *name, int val) {
   if (name) {
     printf("%s: ", name);
--- multiC/tests/test.h
+++ multiC/tests/test.h
@@ -1,6 +1,10 @@
 #ifndef TEST_H
 #define TEST_H
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 /*
  * Print test results. The first parameter is a string that summarizes the test.
  * The second parameter is the boolean that should be true.
