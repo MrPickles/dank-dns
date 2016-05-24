7c4fd7985149b3fba0cce51236a03d4b459f49a5
Andrew Liu <liu.andrew.x@gmail.com>
2016-05-24 02:03:02 -0400

    Add brief documentation to sample query script

--- query/script.sh
+++ query/script.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# This script shows sample usage for running QPS with the given js scripts.
+# You can also choose to run these as NPM scripts.
+
 rep=( cpmd hkcn nyny sekr sewa )
 
 for r in "${rep[@]}"
 do
   for i in {1..2}
   do 
-    ./qps.js -s 2016-03-07 -e 2016-03-08 -i 60 -r $r
+    node qps.js -s 2016-03-07 -e 2016-03-08 -i 60 -r $r
     echo "--- $i of $r ---"
   done
 done
